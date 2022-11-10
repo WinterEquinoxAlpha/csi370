0ac3b222476895a25dc273e6b2b57760d7f0da56
WinterEquinoxAlpha <michael.kashian@mymail.champlain.edu>
2022-11-10 14:16:10 -0500

    Finished assignment 9

--- Assignment9/Main.cpp
+++ Assignment9/Main.cpp
@@ -23,16 +23,43 @@ void printString(char* s)
 
 int main()
 {
-	float a;
-	char s[] = {'H','e','l','l','o',0};
+	float out;
+	char s[] = "Input Float: ";
 
 	__asm {
+		// load the input string into eax then push it onto the stack so it can be called by the printString function
+		lea eax, s
+		push eax
+		call printString
+
+		// initialize the FPU
 		finit
+
+		// load PI into the FPU stack
+		fldpi
+
+		// get a float from the user which is automatically stored on the FPU stack
 		call getFloat
-		fadd ST(0), ST(0)
-		fstp a
+
+		// display the string again then remove the value from the stack as we don't need it anymore
+		call printString
+		pop eax
+
+		// get another float
+		call getFloat
+
+		// perform the arithmetic sqrt((a+b)*pi
+		fadd ST(0), ST(1)
+		fmul ST(0), ST(2)
+		fsqrt
+
+		// save the result to the variable
+		fstp out
+
+		// push the result to the stack to pass it into the printFloat function then pop the stack to manage memory
+		push out
+		call printFloat
+		pop out
 	}
-	printFloat(a);
-	//_printString(&s[0]);
 	return 0;
 }
